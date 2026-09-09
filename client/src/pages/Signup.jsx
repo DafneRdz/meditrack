0d2db9e6a2fc4a0dbb1d87149b741e5a10479058
Dafne Rodriguez <dafne.rodriguez702@gmail.com>
2026-09-09 14:05:42 -0700

    Update API base URL to live Render backend

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -15,7 +15,7 @@ function Signup() {
     e.preventDefault();
     setError('');
     try {
-      const res = await axios.post('http://localhost:5050/api/auth/signup', {
+      const res = await axios.post('https://meditrack-czy4.onrender.com/api/auth/signup', {
         name,
         email,
         password
